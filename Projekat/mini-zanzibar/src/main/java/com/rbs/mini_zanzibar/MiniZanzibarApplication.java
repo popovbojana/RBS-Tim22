51f13309d96afe2903ffe262ad55e94ddb872ba7
Bojana Popov <96697478+popovbojana@users.noreply.github.com>
2024-06-23 13:24:02 +0200

    Project setup (#1)

---
+++ Projekat/mini-zanzibar/src/main/java/com/rbs/mini_zanzibar/MiniZanzibarApplication.java
@@ -0,0 +1,13 @@
+package com.rbs.mini_zanzibar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniZanzibarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniZanzibarApplication.class, args);
+	}
+
+}
